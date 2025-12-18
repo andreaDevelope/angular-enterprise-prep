223958c6ebb086586201b1ec2ae2b176ee5d0879
AndreaDevelope <andrea_develope@outlook.it>
2025-12-18 03:31:27 +0100

    chore: upgrade node and angular, setup eslint, prettier and repo hygiene

--- eslint.config.js
+++ eslint.config.js
@@ -1,44 +1,60 @@
 // @ts-check
-const eslint = require("@eslint/js");
-const { defineConfig } = require("eslint/config");
-const tseslint = require("typescript-eslint");
-const angular = require("angular-eslint");
+const eslint = require('@eslint/js');
+const { defineConfig } = require('eslint/config');
+const tseslint = require('typescript-eslint');
+const angular = require('angular-eslint');
+const prettier = require('eslint-plugin-prettier');
+const prettierConfig = require('eslint-config-prettier');
 
 module.exports = defineConfig([
   {
-    files: ["**/*.ts"],
+    files: ['**/*.ts'],
     extends: [
       eslint.configs.recommended,
       tseslint.configs.recommended,
-      tseslint.configs.stylistic,
       angular.configs.tsRecommended,
+      prettierConfig, //  disattiva regole ESLint in conflitto con Prettier
     ],
     processor: angular.processInlineTemplates,
+    plugins: {
+      prettier,
+    },
     rules: {
-      "@angular-eslint/directive-selector": [
-        "error",
+      // Prettier diventa regola ESLint (fallisce il lint se non formattato)
+      'prettier/prettier': 'error',
+
+      // Angular conventions
+      '@angular-eslint/directive-selector': [
+        'error',
         {
-          type: "attribute",
-          prefix: "app",
-          style: "camelCase",
+          type: 'attribute',
+          prefix: 'app',
+          style: 'camelCase',
         },
       ],
-      "@angular-eslint/component-selector": [
-        "error",
+      '@angular-eslint/component-selector': [
+        'error',
         {
-          type: "element",
-          prefix: "app",
-          style: "kebab-case",
+          type: 'element',
+          prefix: 'app',
+          style: 'kebab-case',
         },
       ],
     },
   },
   {
-    files: ["**/*.html"],
+    files: ['**/*.html'],
+    ignores: ['src/index.html'], //ESCLUDIAMO index.html dalla formattazione
     extends: [
       angular.configs.templateRecommended,
       angular.configs.templateAccessibility,
+      prettierConfig,
     ],
-    rules: {},
-  }
+    plugins: {
+      prettier,
+    },
+    rules: {
+      'prettier/prettier': 'error',
+    },
+  },
 ]);
